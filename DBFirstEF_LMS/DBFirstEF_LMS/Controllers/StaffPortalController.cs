f9d8fadc3ae8f5ca93fc24bb5ca7bfe52f09bb11
nickle52003 <38731864+nickle52003@users.noreply.github.com>
2018-11-19 18:55:21 -0600

    again
    stuff

---
+++ DBFirstEF_LMS/DBFirstEF_LMS/Controllers/StaffPortalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBFirstEF_LMS.Controllers
+{
+    public class StaffPortalController : Controller
+    {
+        // GET: StaffPortal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
