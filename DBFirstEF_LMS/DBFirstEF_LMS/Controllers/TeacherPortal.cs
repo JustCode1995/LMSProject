5e2154a3de941b6bcbcde7f64dd17ff169865a66
nickle52003 <38731864+nickle52003@users.noreply.github.com>
2018-11-20 20:14:13 -0600

    New login portals
    for admin and teacher

---
+++ DBFirstEF_LMS/DBFirstEF_LMS/Controllers/TeacherPortal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBFirstEF_LMS.Controllers
+{
+    public class TeacherPortalController : Controller
+    {
+        // GET: TeacherPortal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DBFirstEF_LMS/DBFirstEF_LMS/StaffsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DBFirstEF_LMS.Models;
+
+namespace DBFirstEF_LMS
+{
+    public class StaffsController : Controller
+    {
+        private LMSDBEntities1 db = new LMSDBEntities1();
+
+        // GET: Staffs
+        public ActionResult Index()
+        {
+            var staffs = db.Staffs.Include(s => s.Department).Include(s => s.StaffLogin);
+            return View(staffs.ToList());
+        }
+
+        // GET: Staffs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Staffs/Create
+        public ActionResult Create()
+        {
+            ViewBag.dept_id = new SelectList(db.Departments, "dept_id", "dept_name");
+            ViewBag.staff_id = new SelectList(db.StaffLogins, "staff_id", "staff_pwd");
+            return View();
+        }
+
+        // POST: Staffs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "staff_id,dept_id,first_name,last_name,date_hired,access_level")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Staffs.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.dept_id = new SelectList(db.Departments, "dept_id", "dept_name", staff.dept_id);
+            ViewBag.staff_id = new SelectList(db.StaffLogins, "staff_id", "staff_pwd", staff.staff_id);
+            return View(staff);
+        }
+
+        // GET: Staffs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.dept_id = new SelectList(db.Departments, "dept_id", "dept_name", staff.dept_id);
+            ViewBag.staff_id = new SelectList(db.StaffLogins, "staff_id", "staff_pwd", staff.staff_id);
+            return View(staff);
+        }
+
+        // POST: Staffs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "staff_id,dept_id,first_name,last_name,date_hired,access_level")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.dept_id = new SelectList(db.Departments, "dept_id", "dept_name", staff.dept_id);
+            ViewBag.staff_id = new SelectList(db.StaffLogins, "staff_id", "staff_pwd", staff.staff_id);
+            return View(staff);
+        }
+
+        // GET: Staffs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Staff staff = db.Staffs.Find(id);
+            db.Staffs.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
