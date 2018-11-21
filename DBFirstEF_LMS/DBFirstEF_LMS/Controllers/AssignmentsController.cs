e1f7850dceaf7db48a2578868859b37389644b56
Caitlin <caitlin.boake@gmail.com>
2018-11-20 19:42:49 -0600

    added controllers
    controller updates

---
+++ DBFirstEF_LMS/DBFirstEF_LMS/Controllers/AssignmentsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DBFirstEF_LMS.Models;
+
+namespace DBFirstEF_LMS.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        private LMSDBEntities1 db = new LMSDBEntities1();
+
+        // GET: Assignments
+        public ActionResult Index()
+        {
+            var assignments = db.Assignments.Include(a => a.Section);
+            return View(assignments.ToList());
+        }
+
+        // GET: Assignments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignment);
+        }
+
+        // GET: Assignments/Create
+        public ActionResult Create()
+        {
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week");
+            return View();
+        }
+
+        // POST: Assignments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "assignment_id,section_id,assignment_name,assignment_due_dt,assignment_open_dt")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assignments.Add(assignment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week", assignment.section_id);
+            return View(assignment);
+        }
+
+        // GET: Assignments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week", assignment.section_id);
+            return View(assignment);
+        }
+
+        // POST: Assignments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "assignment_id,section_id,assignment_name,assignment_due_dt,assignment_open_dt")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assignment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week", assignment.section_id);
+            return View(assignment);
+        }
+
+        // GET: Assignments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignment);
+        }
+
+        // POST: Assignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            db.Assignments.Remove(assignment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DBFirstEF_LMS/DBFirstEF_LMS/Controllers/Student_AssignmentController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DBFirstEF_LMS.Models;
+
+namespace DBFirstEF_LMS.Controllers
+{
+    public class Student_AssignmentController : Controller
+    {
+        private LMSDBEntities1 db = new LMSDBEntities1();
+
+        // GET: Student_Assignment
+        public ActionResult Index()
+        {
+            var student_Assignment = db.Student_Assignment.Include(s => s.Assignment).Include(s => s.Section).Include(s => s.Student);
+            return View(student_Assignment.ToList());
+        }
+
+        // GET: Student_Assignment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student_Assignment student_Assignment = db.Student_Assignment.Find(id);
+            if (student_Assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student_Assignment);
+        }
+
+        // GET: Student_Assignment/Create
+        public ActionResult Create()
+        {
+            ViewBag.assignment_id = new SelectList(db.Assignments, "assignment_id", "assignment_name");
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week");
+            ViewBag.studentID = new SelectList(db.Students, "StudentID", "Fname");
+            return View();
+        }
+
+        // POST: Student_Assignment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "section_id,assignment_id,studentID,grade")] Student_Assignment student_Assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Student_Assignment.Add(student_Assignment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.assignment_id = new SelectList(db.Assignments, "assignment_id", "assignment_name", student_Assignment.assignment_id);
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week", student_Assignment.section_id);
+            ViewBag.studentID = new SelectList(db.Students, "StudentID", "Fname", student_Assignment.studentID);
+            return View(student_Assignment);
+        }
+
+        // GET: Student_Assignment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student_Assignment student_Assignment = db.Student_Assignment.Find(id);
+            if (student_Assignment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.assignment_id = new SelectList(db.Assignments, "assignment_id", "assignment_name", student_Assignment.assignment_id);
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week", student_Assignment.section_id);
+            ViewBag.studentID = new SelectList(db.Students, "StudentID", "Fname", student_Assignment.studentID);
+            return View(student_Assignment);
+        }
+
+        // POST: Student_Assignment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "section_id,assignment_id,studentID,grade")] Student_Assignment student_Assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student_Assignment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.assignment_id = new SelectList(db.Assignments, "assignment_id", "assignment_name", student_Assignment.assignment_id);
+            ViewBag.section_id = new SelectList(db.Sections, "section_id", "day_of_week", student_Assignment.section_id);
+            ViewBag.studentID = new SelectList(db.Students, "StudentID", "Fname", student_Assignment.studentID);
+            return View(student_Assignment);
+        }
+
+        // GET: Student_Assignment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student_Assignment student_Assignment = db.Student_Assignment.Find(id);
+            if (student_Assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student_Assignment);
+        }
+
+        // POST: Student_Assignment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student_Assignment student_Assignment = db.Student_Assignment.Find(id);
+            db.Student_Assignment.Remove(student_Assignment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
